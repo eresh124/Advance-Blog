c7e8fcebc66bce30241434ed3080995534e50ac5
eresh124 <eresh124@gmail.com>
2019-12-11 16:43:29 +0545

    final File

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from PIL import Image
 
 
@@ -10,12 +11,21 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
 
-        img = Image.open(self.image.path)
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        profile = Profile.objects.create(user=kwargs['instance'])
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+
+post_save.connect(create_profile, sender=User)
+
+
+def save(self):
+    super().save()
+
+    img = Image.open(self.image.path)
+
+    if img.height > 300 or img.width > 300:
+        output_size = (300, 300)
+        img.thumbnail(output_size)
+        img.save(self.image.path)
